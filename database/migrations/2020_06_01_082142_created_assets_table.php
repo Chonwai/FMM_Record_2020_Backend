db7e2d8fedefa0375589a842657643102c9845a2
Chonwai Un, Edison <chonwaiun@gmail.com>
2020-06-01 17:07:14 +0800

    Add: Finished the created_assets_table migration.

--- database/migrations/2020_06_01_082142_created_assets_table.php
+++ database/migrations/2020_06_01_082142_created_assets_table.php
@@ -14,8 +14,22 @@ class CreatedAssetsTable extends Migration
     public function up()
     {
         Schema::create('assets', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id')->index();
+            $table->string('name', 100)->index()->nullable();
+            $table->string('description', 100)->nullable();
+            $table->string('category', 100)->nullable();
+            $table->string('state', 100)->nullable();
+            $table->dateTime('acquired_at')->nullable();
+            $table->float('purchase_price')->nullable()->default(0);
+            $table->unsignedInteger('current_value')->nullable()->default(0);
+            $table->string('location', 100)->nullable();
+            $table->string('manufacturer', 100)->nullable();
+            $table->string('model', 100)->nullable();
+            $table->string('comments', 100)->nullable();
+            $table->string('owner', 100)->nullable();
+            $table->string('asset_id', 20)->index()->nullable();
+            $table->string('attachments', 100)->nullable();
+            $table->dateTime('retired_at')->nullable();
         });
     }
 
