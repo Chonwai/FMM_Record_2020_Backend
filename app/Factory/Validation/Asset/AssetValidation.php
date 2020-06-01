48813300460c59ee0f0cdc5271094fa0d90123ca
Chonwai Un, Edison <chonwaiun@gmail.com>
2020-06-01 17:39:05 +0800

    Add: Added the Asset Validation.

---
+++ app/Factory/Validation/Asset/AssetValidation.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Factory\Validation\Record;
+
+use App\Factory\Validation\InterfaceBasic;
+use App\Http\Requests\Asset\AssetsRules;
+use Illuminate\Support\Facades\Validator;
+
+class AssetValidation implements InterfaceBasic
+{
+    /**
+     * Create the Singleton Pattern
+     *
+     * @return object
+     */
+    private static $_instance = null;
+
+    private function __construct()
+    {
+        // Avoid constructing this class on the outside.
+    }
+
+    public static function getInstance()
+    {
+        if (!(self::$_instance instanceof self)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    public function responseAll()
+    {
+        $validator = Validator::make([], AssetsRules::getInstance()->rules());
+        return $validator;
+    }
+
+    public function responseSpecify($request)
+    {
+        $validator = Validator::make(['id' => $request->id], AssetsRules::getInstance()->responseSpecifyRules());
+        return $validator;
+    }
+
+    public function insert($request)
+    {
+        $validator = Validator::make($request->all(), AssetsRules::getInstance()->insertRules());
+        return $validator;
+    }
+
+    public function update($request)
+    {
+        $validator = Validator::make($request->all(), AssetsRules::getInstance()->updateRules());
+        return $validator;
+    }
+
+    public function delete($request)
+    {
+        $validator = Validator::make(['id' => $request->id], AssetsRules::getInstance()->deleteRules());
+        return $validator;
+    }
+}
---
+++ app/Http/Requests/Asset/AssetsRules.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Requests\Asset;
+
+use App\Http\Requests\BaseRules;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AssetsRules extends FormRequest
+{
+    /**
+     * Create the Singleton Pattern
+     *
+     * @return object
+     */
+    private static $_instance = null;
+
+    private function __construct()
+    {
+        // Avoid constructing this class on the outside.
+    }
+
+    public static function getInstance()
+    {
+        if (!(self::$_instance instanceof self)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function responseSpecifyRules()
+    {
+        return [
+            'id' => 'required|exists:assets,id',
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function insertRules()
+    {
+        return [
+            'taken_by' => 'required',
+            'taken_at' => 'required',
+            'staff_number' => 'required',
+            'department' => 'required',
+            'contact' => 'required',
+            'status' => 'required',
+            'is_returned' => 'required',
+            'remark' => 'nullable',
+            'hired_out_by' => 'required',
+            'hired_out_at' => 'date',
+            'returned_by' => 'nullable',
+            'returned_at' => 'nullable|date',
+            'items_records' => 'required',
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function updateRules()
+    {
+        return [
+            'id' => 'exists:records,id',
+            'taken_by' => 'required',
+            'taken_at' => 'required',
+            'staff_number' => 'required',
+            'department' => 'required',
+            'contact' => 'required',
+            'status' => 'required',
+            'is_returned' => 'required',
+            'remark' => 'nullable',
+            'hired_out_by' => 'required',
+            'hired_out_at' => 'date',
+            'returned_by' => 'nullable',
+            'returned_at' => 'nullable|date',
+            'items_records' => 'required',
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function deleteRules()
+    {
+        return [
+            'id' => 'exists:assets,id',
+        ];
+    }
+}
