1458a3352a6939726efddfed44bcb1602130ddcd
Chonwai Un, Edison <chonwaiun@gmail.com>
2020-06-02 16:55:57 +0800

    Add: Added the created_users_table migration.

---
+++ database/migrations/2020_06_02_075128_created_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatedUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('name', 100)->index();
+            $table->string('email', 100)->nullable();
+            $table->string('contact', 100)->nullable();
+            $table->string('staff_number', 100)->nullable();
+            $table->string('department', 100)->index()->nullable();
+            $table->unsignedInteger('sum')->default(0);
+            $table->enum('status', ['internal','external'])->default('external');
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
