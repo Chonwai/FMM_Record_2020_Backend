3e2b47815a6a01bc4365f0efac3d352a0bd05e01
Chonwai Un, Edison <chonwaiun@gmail.com>
2020-06-02 17:06:45 +0800

    Add: Added the UsersRepository.

---
+++ app/Repository/User/UsersRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Repository\User;
+
+use App\Models\Users;
+use App\Repository\InterfaceBasicRepository;
+
+class UsersRepository implements InterfaceBasicRepository
+{
+    /**
+     * Create the Singleton Pattern
+     *
+     * @return object
+     */
+    private static $_instance = null;
+
+    private function __construct()
+    {
+        // Avoid constructing this class on the outside.
+    }
+
+    public static function getInstance()
+    {
+        if (!(self::$_instance instanceof self)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    public function getAll()
+    {
+        $data = Users::orderBy('id')->paginate(20);
+        return $data;
+    }
+
+    public function getSpecify($request)
+    {
+        $data = Users::where('id', '=', $request->id)->get();
+        return $data;
+    }
+
+    public function insert($request)
+    {
+        $data = Users::create($request->all());
+        return $data;
+    }
+
+    public function update($request)
+    {
+        $data = Users::where('id', '=', $request->id)->update($request->all());
+        return $data;
+    }
+
+    public function delete($request)
+    {
+        $data = Users::where('id', '=', $request->id)->delete();
+        return $data;
+    }
+}
