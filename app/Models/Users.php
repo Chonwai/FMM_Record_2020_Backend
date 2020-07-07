45e6b484a58ba1273071e31fb6df87d9061e893e
Chonwai Un, Edison <chonwaiun@gmail.com>
2020-07-07 16:04:40 +0800

    Add: Added the JWT into the Users Model.

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Users extends Model
+class Users extends Authenticatable implements JWTSubject
 {
+    use Notifiable;
+
     public $incrementing = true;
 
     /**
@@ -36,4 +41,24 @@ class Users extends Model
         'created_at' => 'datetime:Y-m-d',
         'updated_at' => 'datetime:Y-m-d',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
