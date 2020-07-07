5c658b2aec4a35eb1ccc469fd5aafd8ca4c6d6b6
Chonwai Un, Edison <chonwaiun@gmail.com>
2020-07-07 18:14:09 +0800

    Add: Added the router middleware.

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,80 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::group(['middleware' => ['jwt.auth']], function() {
+    Route::prefix('v1')->group(function () {
+        /**
+         * Records API ------------------------------------------------------------
+         *
+         * @api
+         */
+        // Get All Records
+        Route::get('records/all', 'Record\RecordsController@responsesAllRecords');
+    
+        // Get Specify Record
+        Route::get('records/{id}', 'Record\RecordsController@responsesSpecifyRecord');
+    
+        // Get Filter Records
+        Route::get('analysis/records/filter', 'Record\RecordsController@responsesFilterRecords');
+    
+        // Get the Amount of Records
+        Route::get('analysis/records/amount', 'Record\RecordsController@responsesAmountRecords');
+    
+        // Insert Record
+        Route::post('records', 'Record\RecordsController@insertRecord');
+    
+        // Update Record
+        Route::put('records/{id}/update', 'Record\RecordsController@updateRecord');
+    
+        // Delete Record
+        Route::delete('records/{id}', 'Record\RecordsController@deleteRecord');
+    
+        /**
+         * Assets API ------------------------------------------------------------
+         *
+         * @api
+         */
+        // Get All Assets
+        Route::get('assets/all', 'Asset\AssetsController@responsesAllAssets');
+    
+        // Get Specify Asset
+        Route::get('assets/{id}', 'Asset\AssetsController@responsesSpecifyAsset');
+    
+        // Insert Asset
+        Route::post('assets', 'Asset\AssetsController@insertAsset');
+    
+        // Update Asset
+        Route::put('assets/{id}/update', 'Asset\AssetsController@updateAsset');
+    
+        // Delete Asset
+        Route::delete('assets/{id}', 'Asset\AssetsController@deleteAsset');
+    
+        /**
+         * Tenants API ------------------------------------------------------------
+         *
+         * @api
+         */
+        // Get All Tenants
+        Route::get('tenants/all', 'Tenant\TenantsController@responsesAllTenants');
+    
+        // Get Specify Tenant
+        Route::get('tenants/{id}', 'Tenant\TenantsController@responsesSpecifyTenant');
+    
+        // Get Specify Tenant By Search Filter
+        Route::get('tenants/search/filter', 'Tenant\TenantsController@responsesSpecifyTenantBySearchFilter');
+    
+        // Insert Tenant
+        Route::post('tenants', 'Tenant\TenantsController@insertTenant');
+    
+        // Update Tenant
+        Route::put('tenants/{id}/update', 'Tenant\TenantsController@updateTenant');
+    
+        // Delete Tenant
+        Route::delete('tenants/{id}', 'Tenant\TenantsController@deleteTenant');
+    });    
+});
 
-Route::prefix('v1')->group(function () {
+Route::prefix('/v1')->group(function () {
     /**
      * Auth API ------------------------------------------------------------
      *
@@ -25,73 +94,4 @@ Route::prefix('v1')->group(function () {
      */
     // User Login API
     Route::post('user/login', 'Auth\AuthController@login');
-
-    /**
-     * Records API ------------------------------------------------------------
-     *
-     * @api
-     */
-    // Get All Records
-    Route::get('records/all', 'Record\RecordsController@responsesAllRecords');
-
-    // Get Specify Record
-    Route::get('records/{id}', 'Record\RecordsController@responsesSpecifyRecord');
-
-    // Get Filter Records
-    Route::get('analysis/records/filter', 'Record\RecordsController@responsesFilterRecords');
-
-    // Get the Amount of Records
-    Route::get('analysis/records/amount', 'Record\RecordsController@responsesAmountRecords');
-
-    // Insert Record
-    Route::post('records', 'Record\RecordsController@insertRecord');
-
-    // Update Record
-    Route::put('records/{id}/update', 'Record\RecordsController@updateRecord');
-
-    // Delete Record
-    Route::delete('records/{id}', 'Record\RecordsController@deleteRecord');
-
-    /**
-     * Assets API ------------------------------------------------------------
-     *
-     * @api
-     */
-    // Get All Assets
-    Route::get('assets/all', 'Asset\AssetsController@responsesAllAssets');
-
-    // Get Specify Asset
-    Route::get('assets/{id}', 'Asset\AssetsController@responsesSpecifyAsset');
-
-    // Insert Asset
-    Route::post('assets', 'Asset\AssetsController@insertAsset');
-
-    // Update Asset
-    Route::put('assets/{id}/update', 'Asset\AssetsController@updateAsset');
-
-    // Delete Asset
-    Route::delete('assets/{id}', 'Asset\AssetsController@deleteAsset');
-
-    /**
-     * Tenants API ------------------------------------------------------------
-     *
-     * @api
-     */
-    // Get All Tenants
-    Route::get('tenants/all', 'Tenant\TenantsController@responsesAllTenants');
-
-    // Get Specify Tenant
-    Route::get('tenants/{id}', 'Tenant\TenantsController@responsesSpecifyTenant');
-
-    // Get Specify Tenant By Search Filter
-    Route::get('tenants/search/filter', 'Tenant\TenantsController@responsesSpecifyTenantBySearchFilter');
-
-    // Insert Tenant
-    Route::post('tenants', 'Tenant\TenantsController@insertTenant');
-
-    // Update Tenant
-    Route::put('tenants/{id}/update', 'Tenant\TenantsController@updateTenant');
-
-    // Delete Tenant
-    Route::delete('tenants/{id}', 'Tenant\TenantsController@deleteTenant');
 });
