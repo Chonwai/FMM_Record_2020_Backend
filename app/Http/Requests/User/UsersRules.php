1370322269d80edc02c0913443a64c54e152bfdc
Chonwai Un, Edison <chonwaiun@gmail.com>
2020-07-14 12:26:05 +0800

    Add: Added the UsersRules.

---
+++ app/Http/Requests/User/UsersRules.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UsersRules extends FormRequest
+{
+    /**
+     * Create the Singleton Pattern
+     *
+     * @return object
+     */
+    private static $_instance = null;
+
+    private function __construct()
+    {
+        // Avoid constructing this class on the outside.
+    }
+
+    public static function getInstance()
+    {
+        if (!(self::$_instance instanceof self)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required|exists:users,id',
+        ];
+    }
+}
